73141e74c07cae5add8a3bd655ae07877cc18d7c
Jason Gedge <jason@gedge.ca>
2017-12-21 16:09:12 -0330

    Add spec for sqlite recorder

--- lib/callgraph/recorders/sqlite.rb
+++ lib/callgraph/recorders/sqlite.rb
@@ -5,51 +5,39 @@ require "sqlite3"
 module Callgraph
   module Recorders
     class Sqlite < Recorder
+      METHODS_TABLE = "methods"
+      METHOD_CALLS_TABLE = "method_calls"
+
+      Method = Struct.new("Method", :name, :class, :path, :line_number, :type)
+      MethodCall = Struct.new("MethodCall", :source, :target)
+
       def initialize(db_path)
         @db_path = db_path
         @stack = []
       end
 
       def record(event)
-        return unless event.type == :call
+        if event.type == :return
+          @stack.pop
+          return
+        end
 
         @stack << store_event(event)
-        return unless event.parent
+        return if @stack.length <= 1
 
         database.execute(
-          "INSERT OR IGNORE INTO method_calls(source, target) VALUES(?, ?)",
-          @stack[-2],
-          @stack[-1]
+          "INSERT OR IGNORE INTO #{METHOD_CALLS_TABLE}(source, target) VALUES(?, ?)",
+          [
+            @stack[-2],
+            @stack[-1],
+          ]
         )
-      end
-
-      private
-
-      def store_event(event)
-        database.execute(
-          "INSERT INTO methods(name, class, path, line_number, type) VALUES(?, ?, ?, ?, ?)",
-          event.method_name.to_s,
-          event.defined_class_name,
-          event.defined_path,
-          event.defined_line_number,
-          event.method_type.to_s
-        )
-        database.last_insert_row_id
-      rescue SQLite3::ConstraintException
-        database.execute(
-          "SELECT id FROM methods WHERE name=? AND class=? AND path=? AND line_number=? AND type=?",
-          event.method_name.to_s,
-          event.defined_class_name,
-          event.defined_path,
-          event.defined_line_number,
-          event.method_type.to_s
-        ).first
       end
 
       def database
         @database ||= SQLite3::Database.new(@db_path).tap do |db|
           db.execute_batch("
-            CREATE TABLE IF NOT EXISTS methods (
+            CREATE TABLE IF NOT EXISTS #{METHODS_TABLE} (
               id integer PRIMARY KEY AUTOINCREMENT,
               name varchar(255),
               class varchar(255),
@@ -58,19 +46,62 @@ module Callgraph
               type varchar(32)
             );
 
-            CREATE UNIQUE INDEX IF NOT EXISTS unique_method ON methods(name, class, path, line_number, type);
-            CREATE INDEX IF NOT EXISTS name ON methods(name);
+            CREATE UNIQUE INDEX IF NOT EXISTS unique_method ON #{METHODS_TABLE}(name, class, path, line_number, type);
+            CREATE INDEX IF NOT EXISTS name ON #{METHODS_TABLE}(name);
 
-            CREATE TABLE IF NOT EXISTS method_calls (
+            CREATE TABLE IF NOT EXISTS #{METHOD_CALLS_TABLE} (
               source integer,
               target integer,
 
               PRIMARY KEY(source, target),
-              FOREIGN KEY(source) REFERENCES methods(id),
-              FOREIGN KEY(target) REFERENCES methods(id)
+              FOREIGN KEY(source) REFERENCES #{METHODS_TABLE}(id),
+              FOREIGN KEY(target) REFERENCES #{METHODS_TABLE}(id)
             );
           ")
         end
+      end
+
+      def methods
+        rows = database.execute("SELECT id, name, class, path, line_number, type FROM #{METHODS_TABLE}")
+        rows.each_with_object({}) do |(id, name, clazz, path, line_number, type), h|
+          h[id] = Method.new(name, clazz, path, line_number.to_i, type.to_sym)
+        end
+      end
+
+      def method_calls
+        return enum_for(:method_calls) unless block_given?
+
+        method_instances = methods
+        database.execute("SELECT source, target FROM #{METHOD_CALLS_TABLE}").each do |source, target|
+          yield MethodCall.new(method_instances[source], method_instances[target])
+        end
+      end
+
+      private
+
+      def store_event(event)
+        database.execute(
+          "INSERT INTO #{METHODS_TABLE}(name, class, path, line_number, type) VALUES(?, ?, ?, ?, ?)",
+          [
+            event.method_name.to_s,
+            event.defined_class_name,
+            event.defined_path,
+            event.defined_line_number,
+            event.method_type.to_s,
+          ]
+        )
+        database.last_insert_row_id
+      rescue SQLite3::ConstraintException
+        database.get_first_value(
+          "SELECT id FROM #{METHODS_TABLE} WHERE name=? AND class=? AND path=? AND line_number=? AND type=?",
+          [
+            event.method_name.to_s,
+            event.defined_class_name,
+            event.defined_path,
+            event.defined_line_number,
+            event.method_type.to_s,
+          ]
+        )
       end
     end
   end
---
+++ spec/callgraph/recorders/sqlite_spec.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Callgraph
+  module Recorders
+    RSpec.describe(Sqlite) do
+      let(:db_path) { "tmp/sqlite_test" }
+
+      # TODO have a shared pool of instance doubles for recorder specs
+      let(:call_event_a) do
+        instance_double(
+          Event,
+          type: :call,
+          method_name: "foo",
+          defined_class_name: "Test",
+          defined_path: "spec/callgraph/recorders/sqlite_spec.rb",
+          defined_line_number: 5,
+          method_type: :class
+        )
+      end
+
+      let(:method_a) { Sqlite::Method.new("foo", "Test", "spec/callgraph/recorders/sqlite_spec.rb", 5, :class) }
+
+      let(:call_event_b) do
+        instance_double(
+          Event,
+          type: :call,
+          method_name: "foo",
+          defined_class_name: "MyCoolClass",
+          defined_path: "spec/callgraph/recorders/sqlite_spec.rb",
+          defined_line_number: 13,
+          method_type: :instance
+        )
+      end
+
+      let(:method_b) do
+        Sqlite::Method.new("foo", "MyCoolClass", "spec/callgraph/recorders/sqlite_spec.rb", 13, :instance)
+      end
+
+      let(:return_event) { instance_double(Event, type: :return) }
+
+      subject { Sqlite.new(db_path) }
+
+      before(:each) { subject.database.transaction }
+      after(:each) { subject.database.rollback }
+
+      describe "#record" do
+        it "writes a single event" do
+          subject.record(call_event_a)
+
+          expect(subject.methods.values).to eq([method_a])
+          expect(subject.method_calls.to_a).to be_empty
+        end
+
+        it "writes multiple events" do
+          subject.record(call_event_a)
+          subject.record(return_event)
+          subject.record(call_event_b)
+
+          expect(subject.methods.values).to eq([method_a, method_b])
+          expect(subject.method_calls.to_a).to be_empty
+        end
+
+        it "writes hierarchical events" do
+          subject.record(call_event_a)
+          subject.record(call_event_b)
+          subject.record(call_event_a)
+          subject.record(return_event)
+          subject.record(return_event)
+          subject.record(call_event_a)
+          subject.record(return_event)
+          subject.record(return_event)
+          subject.record(call_event_b)
+          subject.record(call_event_a)
+
+          expect(subject.methods.values).to eq([method_a, method_b])
+          expect(subject.method_calls.to_a).to contain_exactly(
+            Sqlite::MethodCall.new(method_a, method_b),
+            Sqlite::MethodCall.new(method_b, method_a),
+            Sqlite::MethodCall.new(method_a, method_a)
+          )
+        end
+      end
+    end
+  end
+end
